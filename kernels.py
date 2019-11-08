71901a6e9603c7ca5aeb5c4c392c5bc2815ecef2
bhelfrecht <ben.helfrecht@gmail.com>
2019-11-08 11:45:11 +0100

    Fix kernel centering to be based on reference (training set) kernel

--- kernels.py
+++ kernels.py
@@ -37,12 +37,14 @@ def gaussian_kernel(XA, XB, gamma=1):
     K = np.exp(-gamma*D)
     return K
 
-def center_kernel(K):
+def center_kernel(K, Kref=None):
     """
         Centers a kernel matrix
+        (written with assistance from Michele Ceriotti)
 
         ---Arguments---
         K: the kernel to center
+        Kref: reference (training) kernel
 
         ---Returns---
         Kc: the centered kernel
@@ -53,10 +55,18 @@ def center_kernel(K):
             as a Kernel Eigenvalue Problem, Neural Computation 10, 1299-1319 (1998).
 
     """
-    oneN = np.ones((K.shape[0], K.shape[0]))/K.shape[0]
-    oneM = np.ones((K.shape[1], K.shape[1]))/K.shape[0]
 
-    Kc = K - np.matmul(oneN, K) - np.matmul(K, oneM) \
-            + np.matmul(np.matmul(oneN, K), oneM)
+    if Kref is None:
+        Kref = K
 
-    return Kc
+    if K.shape[1] != Kref.shape[0] or Kref.shape[0] != Kref.shape[1]:
+        print("Error: kernels must have compatible shapes" \
+                + "and the reference kernel must be square")
+    else:
+        oneN = np.ones((K.shape[0], K.shape[0]))/K.shape[1]
+        oneM = np.ones((K.shape[1], K.shape[1]))/K.shape[1]
+
+        Kc = K - np.matmul(oneN, Kref) - np.matmul(K, oneM) \
+                + np.matmul(np.matmul(oneN, Kref), oneM)
+
+        return Kc
