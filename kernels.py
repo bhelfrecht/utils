03245fcef7bb334f0726ddd0505e72778ff973c9
bhelfrecht <ben.helfrecht@gmail.com>
2020-02-05 17:00:11 +0100

    Add versatile kernel building function

--- kernels.py
+++ kernels.py
@@ -5,6 +5,143 @@ import sys
 import numpy as np
 from scipy.spatial.distance import cdist
 
+def build_kernel(XA, XB, XR=None, kernel='linear', gamma=1.0, zeta=1.0):
+    """
+        Build a kernel
+        
+        ---Arguments---
+        XA: XA data; if XA is a list, with element i being an array of environments
+            in structure i, then row i of the kernel matrix will be an
+            an average over environments in structure i
+        XB: XB data; if XB is a list, with element j being an array of environments
+            in structure j, then column j of the kernel matrix will be an
+            average over environments in structure j
+        XR: XR data (Nystrom mode); if XR is provided, a Nystrom approximation
+            of the kernel will be computed. If XR is a list, with element k being
+            an array of environments in structure k, then the column k
+            in the kernel KAR, the row k in the kernel KRB, and the columns
+            and rows in kernel KRR will be an average over environments in structure k
+        kernel: kernel type (linear or gaussian)
+        gamma: gamma (width) parameter for gaussian kernels
+        zeta: zeta (exponent) parameter for linear kernels
+        
+        ---Returns---
+        K: kernel matrix
+    """
+    
+    # Initialize kernel functions and special arguments
+    if kernel == 'gaussian':
+        kernel_func = gaussian_kernel
+        kw = {'gamma': gamma}
+    else:
+        kernel_func = linear_kernel
+        kw = {'zeta': zeta}
+        
+    # Initialize kernel matrices
+    KRR = None
+    KAR = None
+    KRB = None
+    K = None
+
+    # Compute the kernels, where we sum over the axes
+    # corresponding to the data that are provided in lists, 
+    # where each element of a list represents a structure
+    # as an array with the feature vectors of the environments
+    # present in that structure as rows
+    
+    # Nystrom mode
+    if XR is not None:
+        
+        # XR structures
+        if isinstance(XR, list):
+            KRR = np.zeros((len(XR), len(XR)))
+            for rdx1, r1 in enumerate(XR):
+                for rdx2, r2 in enumerate(XR):
+                    KRR[rdx1, rdx2] = np.sum(kernel_func(r1, r2, **kw))/(r1.shape[0]*r2.shape[0])
+        
+            # XA structures, XR structures
+            if isinstance(XA, list):
+                KAR = np.zeros((len(XA), len(XR)))
+                for adx, a in enumerate(XA):
+                    for rdx, r in enumerate(XR):
+                        KAR[adx, rdx] = np.sum(kernel_func(a, r, **kw))/(a.shape[0]*r.shape[0])
+            
+            # XA environments, XR structures
+            else:
+                KAR = np.zeros((XA.shape[0], len(XR)))
+                for rdx, r in enumerate(XR):
+                    KAR[:, rdx] = np.sum(kernel_func(XA, r, **kw), axis=1)/r.shape[0]
+                
+            # XR structures, XB structures
+            if isinstance(XB, list):
+                KRB = np.zeros((len(XR), len(XB)))
+                for rdx, r in enumerate(XR):
+                    for bdx, b in enumerate(XB):
+                        KRB[rdx, bdx] = np.sum(kernel_func(r, b, **kw))/(r.shape[0]*b.shape[0])
+            
+            # XR structures, XB environments
+            else:
+                KRB = np.zeros((len(XR), XB.shape[0]))
+                for rdx, r in enumerate(XR):
+                    KRB[rdx, :] = np.sum(kernel_func(r, XB, **kw), axis=0)/r.shape[0]
+        
+        # XR environments
+        else:
+            KRR = kernel_func(XR, XR, **kw)
+            
+            # XA structures, XR environments
+            if isinstance(XA, list):
+                KAR = np.zeros((len(XA), XR.shape[0]))
+                for adx, a in enumerate(XA):
+                    KAR[adx, :] = np.sum(kernel_func(a, XR, **kw), axis=0)/a.shape[0]
+                    
+            # XA environments, XR environments
+            else:
+                KAR = kernel_func(XA, XR, **kw)
+
+            # XR environments, XB structures
+            if isinstance(XB, list):
+                KRB = np.zeros((XR.shape[0], len(XB)))
+                for bdx, b in enumerate(XB):
+                    KRB[:, bdx] = np.sum(kernel_func(XR, b, **kw), axis=1)/b.shape[0]
+                    
+            # XR environments, XB environments
+            else:
+                KRB = kernel_func(XR, XB, **kw)
+         
+        # Build approximate kernel
+        KRR_inv = np.linalg.inv(KRR)
+        K = np.matmul(KAR, KRR_inv)
+        K = np.matmul(K, KRB)
+              
+    # Normal mode
+    else:
+        
+        # XA structures, XB structures
+        if isinstance(XA, list) and isinstance(XB, list):
+            K = np.zeros((len(XA), len(XB)))
+            for adx, a in enumerate(XA):
+                for bdx, b in enumerate(XB):
+                    K[adx, bdx] = np.sum(kernel_func(a, b, **kw))/(a.shape[0]*b.shape[0])
+
+        # XA structures, XB environments
+        elif isinstance(XA, list):
+            K = np.zeros((len(XA), XB.shape[0]))
+            for adx, a in enumerate(XA):
+                K[adx, :] = np.sum(kernel_func(a, XB, **kw), axis=0)/a.shape[0]
+
+        # XA environments, XB structures
+        elif isinstance(XB, list):
+            K = np.zeros((XA.shape[0], len(XB)))
+            for bdx, b in enumerate(XB):
+                K[:, bdx] = np.sum(kernel_func(XA, b, **kw), axis=1)/b.shape[0]
+
+        # XA environments, XB environments
+        else:
+            K = kernel_func(XA, XB, **kw)        
+
+    return K
+
 def linear_kernel(XA, XB, zeta=1):
     """
         Builds a dot product kernel
