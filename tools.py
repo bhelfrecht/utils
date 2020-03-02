929eada1db65238e43e2999eba08e7969924f5d3
bhelfrecht <ben.helfrecht@gmail.com>
2020-03-02 16:18:29 +0100

    Add sorted eigendecomposition function

--- tools.py
+++ tools.py
@@ -7,8 +7,37 @@ from scipy.sparse.linalg import svds
 from scipy.sparse.linalg import eigsh
 from regression import LR
 
-# TODO: add eigendecomposition function for ordering eigenvalues
-# and eigenvectors and throwing away the tiny ones
+# TODO: sorted svd
+
+def sorted_eigh(X, k=None, tiny=None):
+    # TODO: sparse option
+    """
+        Compute eigendecomposition and sort
+        eigenvalues and eigenvectors in descending order,
+        and remove small eigenvalues (and corresponding eigenvectors)
+
+        ---Arguments---
+        X: matrix to decompose
+        tiny: cutoff for discarding small eigenvalues
+
+        ---Returns---
+        U: sorted eigenvalues
+        V: eigenvectors corresponding to the sorted eigenvalues
+    """
+
+    # Eigendecomposition
+    U, V = np.linalg.eigh(X)
+
+    # Sort in descending order
+    U = np.flip(U, axis=0)
+    V = np.flip(V, axis=1)
+
+    # Remove small eigenvalues and corresponding eigenvectors
+    if tiny is not None:
+        V = V[:, U > tiny]
+        U = U[U > tiny]
+
+    return U, V
 
 def _compute_S(X, Y, alpha=0.0, tiny=1.0E-15, reg=1.0E-15):
     """
