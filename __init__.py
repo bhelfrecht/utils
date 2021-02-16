d566bffadd15a868751cfe0c388734b20f50c0f2
bhelfrecht <ben.helfrecht@gmail.com>
2021-02-16 07:56:22 +0100

    Add init file

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+    Custom utilities for machine learning and processing atomic environments
+"""
+
+__all__ = [
+    'decomposition', 
+    'errors', 
+    'kernels', 
+    'regression', 
+    'selection', 
+    'soap', 
+    'split', 
+    'tools'
+]
